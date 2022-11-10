dc1de4af2d4ed1c89f7ceaa80ca105a61d9ec87c
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-09 21:38:35 -0500

    db seeded

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem "bootsnap", require: false
 
 gem "simple_form", github: "heartcombo/simple_form"
 
+gem "faker"
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "cleaning the DB..."
+Flat.destroy_all
+puts "DB is clean."
+
+puts "Making 4 fake airbnbs..."
+
+4.times do
+  Flat.create!(
+    name: Faker::Marketing.buzzwords,
+    address: Faker::Address.street_address,
+    description: Faker::Quotes::Shakespeare.as_you_like_it_quote,
+    price_per_night: Faker::Commerce.price,
+    number_of_guests: rand(1..12)
+  )
+end
+
+puts "Airbnbs made."
